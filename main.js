1cbae769f4d6766ca453bfdc93b02d843fd692f8
lavvenn <zmievsijkonstantin0@gmail.com>
2024-02-06 07:44:51 +0300

    #

--- main.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function() {
-    var element = document.getElementsByClassName('user-set');
-    var scrollPosition = window.scrollY;
-    element.style.transform = 'translateY(' + scrollPosition + 'px)';
-});
