88fdece83ef25de5d8b012e30ea7bb29fe0222bc
lavvenn <zmievsijkonstantin0@gmail.com>
2024-02-17 21:03:02 +0300

    #

--- main.js
+++ main.js
@@ -7,16 +7,35 @@
 
 // gridd.appendChild(cell);
 
+let counter = 0
+
+
+
+// function changeStyleOnClick() {
+//     var elements = document.querySelectorAll('.clickable'); // выбираем все элементы с классом 'clickable'
+//     elements.forEach(function(element) {
+//         element.addEventListener('click', function() {
+//             this.style.grid_column_start = 1; // изменяем  элемент
+//             this.style.grid_column_start = 2; // изменяем  элемент
+//         });
+//     });
+//     console.log('all oooook')
+// }
+
 function AddElement(TEXT){
+    counter++
     let gridd = document.getElementById("inner");
     let cell = document.createElement("div");
     cell.innerHTML = TEXT;
-    cell.className = 'item';
+    cell.className = 'item' + counter ;
+    // cell.onclick = changeStyleOnClick()
     gridd.appendChild(cell);
 
     setTimeout(function() {
         cell.classList.add('show');
     }, 100);
+
+    console.log(counter)
 }
 
 AddElement('    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos minima quos vel, aliquid corrupti incidunt consectetur error velit, voluptates ipsum odit reprehenderit porro deserunt officiis beatae, quisquam natus odio esse quibusdam ut! Vel, ex aut? Quod sit veniam, perferendis illum nemo consequatur ab similique consectetur impedit, fuga iusto est numquam?')
